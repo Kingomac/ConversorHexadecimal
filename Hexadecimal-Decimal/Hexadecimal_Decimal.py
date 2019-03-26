0a35d4a8a290557e1e5b79011483475534179457
Kingomac <kingomacyt@gmail.com>
2019-03-26 08:01:18 +0100

    Update Hexadecimal_Decimal.py

--- Hexadecimal-Decimal/Hexadecimal_Decimal.py
+++ Hexadecimal-Decimal/Hexadecimal_Decimal.py
@@ -2,22 +2,25 @@ import math
 def Programa():
     hexadecimal = input()
     decimal = 0
-    for cifra in hexadecimal:
-        hexadecimalReves = list(reversed(hexadecimal))
+    for cifra in reversed(list(hexadecimal)):
+        print(list(hexadecimal))
+        print(hexadecimal.index(cifra))
+        [index for index,i in enumerate(hexadecimal) if i==cifra]
+        print(index)
         if cifra.lower() == "a":
-            decimal += 10 * math.pow(16,hexadecimalReves.index(cifra))
+            decimal += 10 * math.pow(16,hexadecimal.index(cifra))
         elif cifra.lower() == "b":
-            decimal += 11 * math.pow(16,hexadecimalReves.index(cifra))
+            decimal += 11 * math.pow(16,hexadecimal.index(cifra))
         elif cifra.lower() == "c":
-            decimal += 12 * math.pow(16,hexadecimalReves.index(cifra))
+            decimal += 12 * math.pow(16,hexadecimal.index(cifra))
         elif cifra.lower() == "d":
-            decimal += 13 * math.pow(16,hexadecimalReves.index(cifra))
+            decimal += 13 * math.pow(16,hexadecimal.index(cifra))
         elif cifra.lower() == "e":
-            decimal += 14 * math.pow(16,hexadecimalReves.index(cifra))
+            decimal += 14 * math.pow(16,hexadecimal.index(cifra))
         elif cifra.lower() == "f":
-            decimal += 15 * math.pow(16,hexadecimalReves.index(cifra))
+            decimal += 15 * math.pow(16,hexadecimal.index(cifra))
         else:
-            decimal += int(cifra) * math.pow(16,hexadecimalReves.index(cifra))
+            decimal += int(cifra) * math.pow(16,hexadecimal.rindex(cifra))
     print(int(decimal))
 
 Programa()
